1e1a3a75680be14a86ba6b8b9198ead038972c4c
vbbay <160340655+vbbay@users.noreply.github.com>
2024-02-19 10:58:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://increased-sherilyn-vabbystark.koyeb.app")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
